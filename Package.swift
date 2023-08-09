167aa87cf307c6ae4799c1a39659d8804252bb35
Gong Zhang <zhanggong@me.com>
2023-08-09 14:47:16 +0800

    Build with Xcode 15 beta 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -8,6 +8,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
+        .visionOS(.v1),
     ],
     products: [
         .library(name: "Introspect", targets: ["Introspect"]),
